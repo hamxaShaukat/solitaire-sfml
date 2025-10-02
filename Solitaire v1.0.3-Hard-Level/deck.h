c2f26a695de93bf3e0cc09f807061f5580875816
Hamza Shaukat <hamzashaukat714@gmail.com>
2025-10-02 18:36:09 +0500

    added more files for hard level

---
+++ Solitaire v1.0.3-Hard-Level/deck.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include "pile.h"
+#include "card.h"
+#include "borrowPile.h"
+#include "stockPile.h"
+#include "foundationPile.h"
+#include "tableauPile.h"
+
+class deck
+{
+public:
+	deck();
+	void reset();
+	void shuffle();
+	std::vector<pile*> getPiles();
+private:
+	std::vector<card> cards;
+};
+
